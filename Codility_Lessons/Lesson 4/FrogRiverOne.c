97f3387735b2ecff18d99344812e06fb3cea193a
Givi Pataridze <pataridzegivi@gmail.com>
2018-02-09 21:52:45 +0300

    Lesson 4 - FrogRiverOne - 100% - O(n)

---
+++ Codility_Lessons/Lesson 4/FrogRiverOne.c
@@ -0,0 +1,27 @@
+//
+//  FrogRiverOne.c
+//  Codility_Lessons
+//
+//  Created by Givi Pataridze on 07.02.2018.
+//  Copyright © 2018 TestProject. All rights reserved.
+//
+
+#include "FrogRiverOne.h"
+
+int solution(int X, int A[], int N) {
+	int B[X+1];
+	for (int i=1; i<=X; i++)
+		B[i] = 0;
+	
+	int counter = 0;
+	for (int i=0; i<N; i++) {
+		if (B[A[i]] == 0)
+		{
+			B[A[i]] = 1;
+			counter++;
+		}
+		if (counter == X)
+			return i;
+	}
+	return -1;
+}
